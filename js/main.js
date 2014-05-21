ca54b8d8b5738289c7b7bd96e366b0e44289c739
Noorain Noorani <noorain.noorani@gmail.com>
2014-05-21 13:38:27 -0400

    fixing spacing issue and removing animations

--- js/main.js
+++ js/main.js
@@ -1,33 +1,20 @@
-$(function() {
-  $(window).scroll(function() {
-    var self = this;
-    this.topOfWindow = $(window).scrollTop();
-    $('.about-column').each(function() {
-    var imagePos = $(this).offset().top;
+// $(function() {
+//   $(window).scroll(function() {
+//     var self = this;
+//     this.topOfWindow = $(window).scrollTop();
+//     $('.about-column').each(function() {
+//     var imagePos = $(this).offset().top;
 
-      if (imagePos < self.topOfWindow+500) {
-        $(this).addClass("animated fadeIn");
-      }
-    });
+//       if (imagePos < self.topOfWindow+500) {
+//         $(this).addClass("animated fadeIn");
+//       }
+//     });
 
-    $('.og-grid > li > a').each(function() {
-    var imagePos = $(this).offset().top;
-      if (imagePos < self.topOfWindow+500) {
-        $(this).addClass("animated fadeInDown");
-      }
-    });
-
-    // $('.wrapper').each(function() {
-    //   if ( self.topOfWindow < $(this).height()) {
-    //     if ($(this).height() > 50 ) {
-    //       var top = $(window).scrollTop();
-    //       $( this ).height( $(this).height() - top);
-    //       window.console.log($(this).height(), top)
-    //     }
-    //   } else {
-    //     $(this).fadeOut();
-    //     window.console.log('hi')
-    //   }
-    // });
-  });
-});
+//     $('.og-grid > li > a').each(function() {
+//     var imagePos = $(this).offset().top;
+//       if (imagePos < self.topOfWindow+500) {
+//         $(this).addClass("animated fadeInDown");
+//       }
+//     });
+//   });
+// });
